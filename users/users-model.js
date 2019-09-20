457afd2583e30fce5052e5b0cacb29d9053225b7
Neha Patel <neha.r.p@gmail.com>
2019-09-20 09:01:05 -0700

    finished creating model for specified routes

--- users/users-model.js
+++ users/users-model.js
@@ -2,7 +2,7 @@ const db = require("../database/dbConfig.js");
 
 module.exports = {
   add,
-  // find,
+  find,
   findBy,
   findById
 };
@@ -14,4 +14,18 @@ function add(user) {
       const [id] = ids;
       return findById(id);
     });
+}
+
+function find() {
+  return db("users").select("id", "username", "password");
+}
+
+function findBy(filter) {
+  return db("users").where(filter);
+}
+
+function findById(id) {
+  return db("users")
+    .where({ id })
+    .first();
 }
