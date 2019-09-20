5326bb252d7a308c733931db4adf95759586a4b8
Neha Patel <neha.r.p@gmail.com>
2019-09-20 10:39:15 -0700

    login route test working

--- auth/auth-router.spec.js
+++ auth/auth-router.spec.js
@@ -4,21 +4,35 @@ const db = require("../database/dbConfig");
 
 describe("auth-router.js", () => {
   describe("auth route", () => {
+    // beforeEach(async () => {
+    //     await db("users").truncate();
+    //   });
+  
     it("should return status 201 from auth register route", async () => {
       const expectedStatus = 201;
 
       const response = await request(server)
         .post("/api/auth/register")
-        .send({ username: "daddy", password: "patel" });
+        .send({ username: "rahul", password: "patel" });
         
       expect(response.status).toBe(expectedStatus);
     });
 
-    it("shoudl return a JSON object", async () => {
+    it("should return a JSON object", async () => {
         const expectedBody = await db('users');
 
         const response = await request(server).get("/")
         expect(response.body.username).toEqual(expectedBody.username)
+    })
+
+    it("should return status 200 for login route", async () => {
+        const expectedStatus = 200;
+        
+        const response = await request(server)
+        .post("/api/auth/login")
+        .send({username: "rahul", password: "patel"})
+
+        expect(response.status).toBe(expectedStatus)
     })
 
   });
