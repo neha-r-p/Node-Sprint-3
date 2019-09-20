d95d200b175ba8dc59ef86c2c65ee3fd4364a5b7
Neha Patel <neha.r.p@gmail.com>
2019-09-20 09:45:30 -0700

    created one test for index route

--- api/server.spec.js
+++ api/server.spec.js
@@ -3,5 +3,12 @@ const request = require('supertest')
 const server = require('./server.js')
 
 describe("server.js", () => {
-    describle("index route", () =>)
+    describe("index route", () => {
+        it("should return status 200 from index route", async () => {
+            const expectedStatus = 200;
+
+            const response = await request(server).get("/")
+            expect(response.status).toEqual(expectedStatus);
+        })
+    })
 })
