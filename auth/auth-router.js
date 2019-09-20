5734d7fd90ac688d847e844b390bf6771696dd3a
Neha Patel <neha.r.p@gmail.com>
2019-09-20 09:14:23 -0700

    created generateToken function and secrets.js

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -3,22 +3,23 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 const Users = require("../users/users-model.js");
+const secrets = require("./secrets.js");
 
 router.post("/register", (req, res) => {
   let newUser = req.body;
-  
+
   const hash = bcrypt.hashSync(newUser.password, 12);
 
   newUser.password = hash;
 
   Users.add(newUser)
     .then(saved => {
-      res.status(201).json(saved)
+      res.status(201).json(saved);
     })
     .catch(err => {
-      console.log("register err", err)
-      res.status(500).json({ error: "Error creating new user" })
-    })
+      console.log("register err", err);
+      res.status(500).json({ error: "Error creating new user" });
+    });
 });
 
 router.post("/login", (req, res) => {
@@ -26,15 +27,26 @@ router.post("/login", (req, res) => {
   let { username, password } = req.body;
 
   Users.findBy({ username })
-  .first()
-  .then(user => {
-    if(user && bcrypt.compareSync(password, user.password)) {
-      const token = generateToken(user);
-      res.status(200).json({ token })
-    } else {
-      res.status(401).json({ error: "You shall not pass!" })
-    }
-  })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({ token });
+      } else {
+        res.status(401).json({ error: "You shall not pass!" });
+      }
+    });
 });
+
+function generateToken(user) {
+  const payload = {
+    username: user.username
+  };
+  const options = {
+    expiresIn: "1d"
+  };
+
+  return jwt.sign(payload, secrets.jwtSecret, options);
+}
 
 module.exports = router;
